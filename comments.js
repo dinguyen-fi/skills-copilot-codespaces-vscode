e612f68b23944609708c77da4f53f38a814715a9
Diem Nguyen <di.nguyen@fi.com>
2025-08-27 13:22:15 -0700

    edit comments request

--- comments.js
+++ comments.js
@@ -1,6 +1,13 @@
-// create web server
+// Create web server for comment
 //============================================
 
 // Import module
 const express = require('express');
 const router = express.Router();
+const commentController = require('../controllers/comments.js');
+
+// Handle request
+router.get('/', commentController.comment_list);
+router.get('/create', commentController.comment_create_page);
+router.post('/create', commentController.comment_create_page);
+router.get('/:id/delete', commentController.comment_delete);
