61dc2bdbe286364148b2e9d0f80387e63b7e58e8
Diem Nguyen <di.nguyen@fi.com>
2025-08-27 13:16:53 -0700

    copilot not working commit

--- member.js
+++ member.js
@@ -1,5 +1,13 @@
-function skillsMember() {
-    console.log("member.js adding");
-
-
+function skillsMember(){
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'skillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '*'
+        }
+    };
 }
+    
